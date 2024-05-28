15a238352fd951d82212f99d782faf9044e5bada
Tomás Sale <tomassale@hotmail.com>
2024-05-27 23:28:44 -0300

    Done 5.1.5

--- ejercicios/Iteración definida/5.1.5.c
+++ ejercicios/Iteración definida/5.1.5.c
@@ -3,12 +3,19 @@ Confeccionar un programa para calcular la suma de los primeros N números natural
 solicita por teclado el programa.
 ***************************************************************************************************/
 
+#include <stdio.h>
+
 int main(){
+    int natural;
+    int acumulador = 0;
 
-    int N, suma = 0;
+    printf("Seleccione un numero positivo: ");
+    scanf("%d", &natural);
 
+    for(int i = 0; i <= natural ;i++){
+        acumulador += i;
+    }
 
-
-
+    printf("El numero acumulado es: %d", acumulador);
     return 0;
 }
