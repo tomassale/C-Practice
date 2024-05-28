358e756d02d062ab62171b6f3c23ac356b8e9e90
Tomás Sale <tomassale@hotmail.com>
2024-05-28 20:01:41 -0300

    Done 5.1.6/5.1.7/5.1.8/5.1.9 & Started 5.1.10

--- ejercicios/Iteración definida/5.1.10.c
+++ ejercicios/Iteración definida/5.1.10.c
@@ -1,13 +1,33 @@
 /********************************************************************************************************
 Confeccionar un programa que solicite el ingreso de un valor entero N < 12 y luego una lista de N números
 reales sobre la cual debe calcular:
-a. el promedio de los positivos.
-b. el promedio de los negativos.
-c. la cantidad de ceros.
+    a. el promedio de los positivos.
+    b. el promedio de los negativos.
+    c. la cantidad de ceros.
 Si el valor ingresado N es mayor que 12 informar 'VALOR EXCEDIDO” y si es menor o igual a 0 informar
 “CANTIDAD INVALIDA”.
 ********************************************************************************************************/
 
+#include <stdio.h>
+
 int main(){
+
+    int promedioPositivos, promedioNegativo;
+    int cerosEnteros= 0;
+    int cerosDecimal = 0;
+    float numero = 1020304050.214567;
+
+    //if(numero>12)printf("VALOR EXCEDIDO");
+    //if(numero<=0)printf("CANTIDAD INVALIDA");
+
+    int parte_entera = (int)numero;
+    float parte_decimal = numero - (int)numero;
+
+    for(;0;){
+
+    }
+
+    printf("%f", parte_decimal);
+
     return 0;
 }
--- ejercicios/Iteración definida/5.1.9.c
+++ ejercicios/Iteración definida/5.1.9.c
@@ -3,6 +3,20 @@ Confeccionar un programa que exhiba por pantalla una lista, a dos columnas, con 
 impares en la primera y los 15 primeros pares en la segunda, incluyendo los títulos.
 ******************************************************************************************************/
 
-int main(){
+#include <stdio.h>
+
+int main() {
+    int impares = 1;
+    int pares = 0;
+
+    printf("IMPARES PARES\n");
+    for(int i = 0;i <= 15;i += 2){
+
+       printf("%d\t%d\n", impares, pares);
+
+       pares += 2;
+       impares += 2;
+    }
+
     return 0;
 }
