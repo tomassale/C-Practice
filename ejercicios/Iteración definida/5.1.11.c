01db444ea4a1a7cc286dc54fe6687723ed937812
Tomás Sale <tomassale@hotmail.com>
2024-06-03 14:41:44 -0300

    To fix 5.1.10

--- ejercicios/Iteración definida/5.1.11.c
+++ ejercicios/Iteración definida/5.1.11.c
@@ -3,6 +3,22 @@ Ingresar 18 valores de temperatura distintos de cero. Se pide determinar e infor
 valores seguidos) de valores positivos y cuantas de negativos hay.
 *******************************************************************************************************/
 
+#include <stdio.h>
+#include <math.h>
+
 int main(){
+
+    int temp;
+
+    if(temp == 0){
+        printf("Tiene que ser un numero distinto de 0");
+        return 0;
+    }else{
+        for(temp ;){
+
+        }
+    }
+
+
     return 0;
 }
